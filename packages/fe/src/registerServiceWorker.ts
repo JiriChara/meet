8f6a324f9bbe49937001f9e790a51965c140be63
Raffaele Pizzari <raffaele.pizzari@gmail.com>
2019-03-11 21:38:16 +0100

    Adds Vue app

--- packages/fe/src/registerServiceWorker.ts
+++ packages/fe/src/registerServiceWorker.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-console */
+/* tslint:disable:no-console */
 
 import { register } from 'register-service-worker';
 
@@ -6,8 +6,8 @@ if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log(
-        'App is being served from cache by a service worker.\n'
-        + 'For more details, visit https://goo.gl/AFskqB',
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB',
       );
     },
     registered() {
